4df7b1d8cfbeb75f2094ce5f7b7dc8c2b50795f7
TortongDev <kittithat.charoenkhet101@gmail.com>
2024-05-16 21:37:06 +0700

    update v1

--- App/Migration/AppMigration.php
+++ App/Migration/AppMigration.php
@@ -14,20 +14,32 @@
             $field = array();
             $type = array();
             $capsult = array();
+            $auto_increment = array();
             foreach ($arrayField as $nameField => $value) {
                 $field[] = array($nameField);
             }
             foreach ($arrayField as $key => $value) {
                 $type[] = array($value['type']);
-                // $field .= $value['type'];
+            }
+            foreach ($arrayField as $key => $value) {
+                $length[] = array($value['length']);
+            }
+            foreach ($arrayField as $key => $value) {
+                $auto_increment[] = array($value['auto_increment']);
             }
             array_push($capsult, $field);
             array_push($capsult, $type);
-            
-           $this->prePrint($capsult);
-
-            // $field = substr($field,0,-1);
-            // echo $field;
+            array_push($capsult, $length);
+            array_push($capsult, $auto_increment);
+            $auto_true = "";
+            if($capsult[3][$key][0] == 1){
+                $auto_true = " auto_increment primary Key ";
+            }
+            foreach ($capsult as $key => $value) {
+                $statement .= $capsult[0][$key][0].' '.$capsult[1][$key][0]."(".$capsult[2][$key][0].")" . $auto_true  .",";
+            }
+           $field = substr($statement,0,-1);
+           echo $field;
         }
     }
     $migrate  = new AppMigration();
