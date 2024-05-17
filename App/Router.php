c6ba3b8addc8e4494e16917a0a4f3cf31b7d8c1a
TortongDev <kittithat.charoenkhet101@gmail.com>
2024-05-17 17:24:21 +0700

    update structure

--- App/Router.php
+++ App/Router.php
@@ -6,10 +6,6 @@
     $router->setBasePath('/PTWork/App');
     
     
-    
-    
-    
-    
     $router->map( 'GET', '/', function() {
         echo "PTWork";
     });
@@ -22,12 +18,5 @@
         $user->store1($id);
     });
 
-    $match = $router->match();
-        // call closure or throw 404 status
-     if( is_array($match) && is_callable( $match['target'] ) ) {
-        call_user_func_array( $match['target'], $match['params'] );
-     } else {
-        // no route was matched
-        //  header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
-     }
+   
 ?>
--- App/index.php
+++ App/index.php
@@ -1,5 +1,12 @@
 <?php
    
     require_once "./Router.php";
-   
+    $match = $router->match();
+        // call closure or throw 404 status
+     if( is_array($match) && is_callable( $match['target'] ) ) {
+        call_user_func_array( $match['target'], $match['params'] );
+     } else {
+        // no route was matched
+        //  header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+     }
 ?>
